de9a16468fce381c36e42e45e9f50cce27670059
Harsh Singh <harshs08@gmail.com>
2016-11-07 00:25:32 -0800

    adds twitter controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
-
+  def index
+    if user_signed_in?
+      redirect_to twitter_index_path
+    end
+  end
 end
---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,6 @@
+class TwitterController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+end
