efc8f61a41e155c924ae0ef43bfb4fe09897a5de
Harsh Singh <harshs08@gmail.com>
2016-11-07 11:11:52 -0800

    adds twitter views

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,12 +2,14 @@ class TwitterController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @username ||= params[:username]
+    show_tweets if @username.present?
   end
+
+  private
   
   def show_tweets
-    @username = params[:username] || 'harshs08'  
-    @tweets = cache("twitter_results_#{@username}", expires_in: 5.minutes) do
+    @tweets = Rails.cache.fetch("#{@username}/show_tweets", expires_in: 5.minutes) do
       TwitterService.tweets(usr: @username)
     end
   end
